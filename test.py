23260b4c94488bcbee468b6ea848af7597d2358a
marcin <mbburdzy@gmail.com>
2020-01-13 19:27:49 +0100

    _

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.DataFrame({'foo': ['one', 'kl'],
+                   'bar': ['A', 'B'],
+                   'baz': [1, 3],
+                   'zoo': [2, 3]})
+
+vb = {'x': df.iloc[:, 3].to_list()}
+vb_ = {'c': df.iloc[:, 2].to_list()}
+
+print(
+    {**vb, **vb_}
+)
