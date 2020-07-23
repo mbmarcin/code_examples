947c5eadfdbf3e0c5d39dfe691ec171275972c00
mbmarcin <47281320+mbmarcin@users.noreply.github.com>
2020-07-23 22:18:26 +0200

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,6 +1,6 @@
 import datetime
 
-
+#====================================================================================
 def time_decorator(func):
     def inner1():
         print(f"Start time is : {datetime.datetime.now()}")
@@ -14,8 +14,37 @@ def time_decorator(func):
 @time_decorator
 def test_decorator_simple():
     print(f"Time in test : {datetime.datetime.now()}")
+#=========================================================================================
 
+class decorator_with_arguments(object):
 
+    def __init__(self, arg1, arg2, arg3):
+        """
+        If there are decorator arguments, the function
+        to be decorated is not passed to the constructor!
+        """
+        print("Inside __init__()")
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+
+    def __call__(self, f):
+        """
+        If there are decorator arguments, __call__() is only called
+        once, as part of the decoration process! You can only give
+        it a single argument, which is the function object.
+        """
+        print("Inside __call__()")
+        def wrapped_f(*args):
+            print("Inside wrapped_f()")
+            print("Decorator arguments:", self.arg1, self.arg2, self.arg3)
+            f(*args)
+            print("After f(*args)")
+        return wrapped_f
+
+@decorator_with_arguments("Donald", "Le", "1990")
+def test_decorator_with_arguments():
+    print("Testing decorator with arguments")
 
 # def ordinary():
 #     print("I am an ordinary function.")
